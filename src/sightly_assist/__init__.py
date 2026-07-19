f1b75577ffbc660d50827b9d5c422f6a9f427a4a
WoodyJr13 <emailveds@gmail.com>
2026-07-18 20:22:53 -0400

    Add package initialization

---
+++ src/sightly_assist/__init__.py
@@ -0,0 +1,6 @@
+"""Sightly Assist research package."""
+
+from sightly_assist.schemas import RiskAssessment, ScenarioConfig, Vector2
+
+__all__ = ["RiskAssessment", "ScenarioConfig", "Vector2"]
+__version__ = "0.1.0"
